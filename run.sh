cd771e5de59943d55e1ae993c1b0a74640023d72
Ramon Rodrigues <cerberus.ramon@gmail.com>
2014-08-25 11:57:32 +0100

    Github api finished

--- run.sh
+++ run.sh
@@ -1,52 +1,56 @@
 # #!/bin/bash
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
 #Regular expression
-
 regex="[a-zA-Z]*";
 
 #Check if file with token exists
-
 if [ -f token ]; then
-	gitToken=$(<token)
-	echo "Git token: $gitToken";
+  gitToken=$(<gitHubToken)
+  echo "Git token: $gitToken";
 else
 
-	read -p "Insert Authorization token for Github: " gitToken;
-	echo $gitToken > token;
+  read -p "Insert Authorization token for Github: " gitToken;
+  echo $gitToken > token;
 fi
-
-curl -H "Authorization: token $gitToken " https://api.github.com/user/repos | grep "\"language\": \"[a-zA-Z]*\"" > language
 
 for OUTPUT in $(curl -H "Authorization: token $gitToken " https://api.github.com/user/repos | grep -o git@[a-zA-Z0-9:._\/]*)
 do
-	ls --color
 
-	#The repo exists in the config file
-	if [ -f repoConfig ]; then
-	    folderName=$(grep "$regex git@github.com:megajpc/colisoes.git" repoConfig)
-	    folderName=$(echo $folderName | grep -E -o "^[a-z]+")
-	    
-	    #Check if the folder is created or not
-	    if [ -d $folderName ]; then
-				cd $folderName
-			else
-				read -p "Which folder should I save $OUTPUT? " complete -F folderName;
-			fi
+  #Check if there exists a repo config file
+  if [ -f repoConfig ]; then
+    folderName=$(grep "$regex $OUTPUT" repoConfig)
+    folderName=$(echo $folderName | grep -E -o "^[a-z]+")
+    
+    #Check if repo has a folder assigned
+    if [ -z $folderName ]; then
 
-			echo "git clone $OUTPUT";
-			cd ..
-	fi 
-		
-	read -p "Which folder should I save $OUTPUT? " complete -F folderName;
+      echo "Which folder should I save $OUTPUT?"
+      read folderName
+      #Save option
+      echo "$folderName $OUTPUT" >> repoConfig
+    fi
 
-		
-	if [ -d $folderName ]; then
-		cd $folderName
-	else
-		mkdir $folderName && cd $folderName
-	fi
+    #Check if folder already exists
+    if [ -d $folderName ]; then
 
-	echo "git clone $OUTPUT";
-	cd ..
+      cd $folderName
+      echo "git clone $OUTPUT"
+    else
 
+      mkdir -m 777 $folderName && cd $folderName
+      echo "git clone $OUTPUT"
+
+    fi
+    cd ..
+  else
+
+    echo "Which folder should I save $OUTPUT?"
+    read folderName
+    #Save option
+    echo "$folderName $OUTPUT" >> repoConfig
+    mkdir -m 777 $folderName && cd $folderName
+    echo "git clone $OUTPUT"
+    cd ..
+  fi
 done
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-regex="[a-zA-Z]*"
-
-if [ -f repoConfig ]; then
-    directory=$(grep "$regex git@github.com:megajpc/colisoes.git" repoConfig)
-    directory=$(echo $directory | grep -E -o "^[a-z]+")
-    echo $directory
-fi  
